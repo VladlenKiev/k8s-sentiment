acd615e4659f741db9c65a1af546fb61eac37ce5
vladlenkiev <v2700469>
2022-11-19 09:37:56 +0100

    add logger to sa-web

--- sa-webapp/src/main/java/com/sa/web/SentimentController.java
+++ sa-webapp/src/main/java/com/sa/web/SentimentController.java
@@ -2,13 +2,22 @@ package com.sa.web;
 
 import com.sa.web.dto.SentenceDto;
 import com.sa.web.dto.SentimentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.WebRequest;
+
+import java.net.UnknownHostException;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class SentimentController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SentimentController.class);
 
     @Value("${sa.logic.api.url}")
     private String saLogicApiUrl;
@@ -16,20 +25,25 @@ public class SentimentController {
     @PostMapping("/sentiment")
     public SentimentDto sentimentAnalysis(@RequestBody SentenceDto sentenceDto) {
         RestTemplate restTemplate = new RestTemplate();
-//        SentimentDto sentimentDto = new SentimentDto();
-//        sentimentDto.setSentence(sentenceDto.getSentence());
-//        sentimentDto.setPolarity(125f);
-//        System.out.println("sentimentDto = " + sentimentDto);
-//        return sentimentDto;
-        System.out.println("Incoming message = " + sentenceDto);
-        return restTemplate.postForEntity(saLogicApiUrl + "/analyse/sentiment",
-                sentenceDto, SentimentDto.class)
-                .getBody();
+        LOGGER.info("Incoming message ='{}'", sentenceDto);
+        SentimentDto response = restTemplate.postForEntity(saLogicApiUrl + "/analyse/sentiment",
+                sentenceDto, SentimentDto.class).getBody();
+        LOGGER.info("Incoming message {} has RESPONSE = {}", sentenceDto, response);
+        return response;
     }
 
     @GetMapping("/testHealth")
     public String testHealth() {
         return "all ok";
+    }
+
+    @ExceptionHandler(value
+            = {UnknownHostException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        LOGGER.error("Incoming request '{}' has error {}", request, ex.getMessage());
+
+        return new ResponseEntity(bodyOfResponse, HttpStatus.BAD_REQUEST);
     }
 }
 
